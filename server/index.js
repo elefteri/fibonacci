d7190830d092a07531ea351a4fe66873317c849e
elefteri metafrasi <elefteri.metafrasi@outlook.com>
2020-03-12 16:28:20 +0200

    redis ready

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const keys = require('./keys');
-const redis = require('redis');
 
 // Express app setup
 const express = require('express');
@@ -25,3 +24,12 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 pgClient.query('CREATE TABLE IF NOT EXISTS values (NUMBER INT)')
         .catch(err => console.log(err));
 
+//redis client setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
