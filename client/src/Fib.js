f09b297c859be6a86ff5314434d58fb7da30ee5d
elefteri metafrasi <elefteri.metafrasi@outlook.com>
2020-03-12 17:42:19 +0200

    react ready?

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -23,4 +23,50 @@ class Fib extends Component {
         const indexes = await axios.get('/api/values/all');
         this.setState({ seenIndexes: indexes.data });
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post('/api/values', {index: this.state.index});
+        this.setState({ index: '' });
+    }
+
+    renderSeenIndexes() {
+        return this.state.seenIndexes
+                         .map( ({FIB_INDEX}) => FIB_INDEX )
+                         .join(', ');
+    }
+
+    rendercaclulatedVaules() {
+        const entries = [];
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} the Caclulated Fib is {this.state.values[key]}
+                </div>
+            );
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <div>
+                    <form onSubmit = {this.handleSubmit}>
+                        <label>Enter your index</label>
+                        <input value = {this.state.index}
+                               onChange = {event => this.setState({ index: event.target.value })} />
+                        <button>Submit</button>
+                    </form>
+                </div>
+                <div>
+                    <h3>Indexes I've Seen:</h3>
+                    {this.renderSeenIndexes()}
+                </div>
+                <div>
+                    <h3>Caclulated Values:</h3>
+                    {this.rendercaclulatedVaules()}
+                </div>
+            </div>
+        );
+    }
 }
