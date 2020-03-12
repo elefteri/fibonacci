4c7101ed7d554f7de0e95bdf90f5e63c7c3f9a34
elefteri metafrasi <elefteri.metafrasi@outlook.com>
2020-03-12 17:55:01 +0200

    before the error

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Otherpage from './OtherPage';
+import Fib from './Fib';
+
 import logo from './logo.svg';
 import './App.css';
+import OtherPage from './OtherPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 className="App-title">Welcome to Fibonacci</h2>
+          <Link to = "/">Home</Link>
+          <Link to = "/otherPage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path = "/" component = {Fib} />
+          <Route path = "/otherPage" component = {OtherPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
